80cea205ba9907be9206d116dd4a650a71bb1165
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-10-13 17:17:57 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,19 @@
 class Show < ActiveRecord::Base
-  
+  def highest_rating
+    Show.maximum("rating")
+  end
+
+  def most_popular_show
+    Show.where("rating = ?", highest_rating).first
+  end
+
+  def lowest_rating
+    Show.minimum("rating")
+  end
+
+  def least_popular_show
+    Show.where("rating = ?", lowest_rating).first
+  end
+
+  def
 end
